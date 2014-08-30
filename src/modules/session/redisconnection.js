4bf6dc312d0fd028399eb3a0c7e5f166f9b35495
reaperes <emerald105@hanmail.net>
2014-08-30 21:20:35 +0900

    update redis-connection module. If connection refused, automatically try to reconnect.

--- src/modules/session/redisconnection.js
+++ src/modules/session/redisconnection.js
@@ -1,29 +1,35 @@
 'use strict';
-var redis = require('redis');
+(function() {
+  var redis = require('redis');
+  var errors = require(SOURCE_ROOT + '/errors.js');
 
-var errors = require(SOURCE_ROOT+'/errors.js');
+  var redisconnection = {};
 
+  var client = redis.createClient(GLOBAL.db.port, GLOBAL.db.host);
+  var connected = false;
+  var errorCount = 0;
 
-var client = redis.createClient(GLOBAL.db.port, GLOBAL.db.host);
-var connected = false;
-var errorCount = 0;
+  client.on('error', function (err) {
+    connected = false;
+    console.log('Redis connection ' + errorCount++ + ' error: ' + err);
+    console.log('Trying to reconnect...');
+    setTimeout(function () {
+      if (errorCount > 10) throw new errors.RedisConnectionError('Error: Can not connect redis.');
+      client = redis.createClient(GLOBAL.db.port, GLOBAL.db.host);
+    }, 1000);
+  });
+  client.on('connect', function () {
+    console.log('Redis connected: ' + new Date());
+    connected = true;
+  });
 
-client.on('error', function(err) {
-  connected = false;
-  console.log('Redis connection ' + errorCount++ + ' error: ' + err);
-  console.log('Trying to reconnect...');
-  setTimeout(function() {
-    if (errorCount > 10) throw new errors.RedisConnectionError('Error: Can not connect redis.');
-    client = redis.createClient(GLOBAL.db.port, GLOBAL.db.host);
-  }, 1000);
-});
-client.on('connect', function() {
-  connected = true;
-});
+  redisconnection.getConnection = function() {
+    if (connected)
+      return client;
+    return null;
+  };
 
+  module.exports = redisconnection;
 
-module.exports.getConnection = function() {
-  if (connected)
-    return client;
-  return null;
-};
+  return this;
+})();
